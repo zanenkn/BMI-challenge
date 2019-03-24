2348f6ca203ec4bec8ea1063e1ee629aa4725c04
zanenkn <zane.neikena@gmail.com>
2019-03-24 13:48:41 +0100

    adds calculator_spec.js and calculator.js

--- spec/spec.helper.js
+++ spec/spec.helper.js
@@ -2,3 +2,10 @@ const chai = require('chai');
 const BrowserHelpers = require('e2e_training_wheels')
 global.browser = new BrowserHelpers()
 global.expect = chai.expect;
+
+
+const fs = require('fs');
+
+let calc = fs.readFileSync('./src/js/calculator.js');
+let person = fs.readFileSync('./src/js/person.js');
+eval(calc + person + `\nexports.Person = Person;\nexports.BMICalculator = BMICalculator;`)
