b7b830e84c1334701e3c1a386f9d2f2e0cc1201c
zanenkn <zane.neikena@gmail.com>
2019-03-26 13:48:00 +0100

    adds test for imperial method on index.features.js, adjusts index.html before implementing imperial method. test for metric method green.

--- features/index.feature.js
+++ features/index.feature.js
@@ -16,13 +16,26 @@ describe('User can calculate BMI', () => {
         await browser.close();
     });
 
-    it('by inputing his weight and height', async () => {
+    it('using metric method by inputing his weight and height', async () => {
 
         await browser.fillIn("input[id='weight-in-kgs']", { with: "95" })
         await browser.fillIn("input[id='height-in-cm']", { with: "185" })
 
-        await browser.clickOnButton("button")
-        let content = await browser.getContent("span[id='display_value']")
+        await browser.clickOnButton("button[id='metric']")
+        let content = await browser.getContent("span[id='display_metric_value']")
         expect(content).to.eql('Your BMI is 27.76');
     });
+
+    it('using imperial method by inputing his weight and height', async () => {
+
+        await browser.fillIn("input[id='weight-in-lbs']", { with: "180" })
+        await browser.fillIn("input[id='height-in-ft']", { with: "5" })
+        await browser.fillIn("input[id='height-in-in']", { with: "11" })
+
+        await browser.clickOnButton("button[id='imperial']")
+        let content = await browser.getContent("span[id='display_imperial_value']")
+        expect(content).to.eql('Your BMI is 25.1');
+});
+
+
 });
